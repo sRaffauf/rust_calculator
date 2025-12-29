dbecf93b3aaa3654b1f681da305b6dd2a5ae9fa5
sraffauf <schn33fuchs@web.de>
2025-12-29 15:38:44 +0100

    Added input parsing for numbers

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,29 @@
 use std::io;
 
 fn main() {
-    let mut result: f64;
-    let mut user_input = String::new();
-    println!("Input Number");
-    get_user_input(&mut user_input);
+    let mut result = get_number_input();
+    loop {
+        let operation = get_string_input();
+
+    }
 }
 
-fn get_user_input(buffer: &mut String) {
-    buffer.clear();
-    match io::stdin().read_line(buffer) {
-        Ok(_) => {},
-        Err(err) => println!("{}", err)
-    }
-} 
+fn get_number_input() -> f64 {
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf).expect("Failed to read input");
+    let x: f64 = match buf.trim().parse() {
+        Ok(num) => num,
+        Err(_) => f64::NAN
+    };
+    return x;
+}
+
+fn get_string_input() -> String {
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf).expect("Failed to read input"); 
+    let x: String = match buf.trim().parse() {
+        Ok(str) => str,
+        Err(_) => String::new()
+    };
+    return x;
+}
