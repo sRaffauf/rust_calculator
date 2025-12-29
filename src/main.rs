c175ada6d0b22344bea5765c28c089ec415e9063
sraffauf <schn33fuchs@web.de>
2025-12-29 15:19:44 +0100

    Implemented basic input reading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut result: f64;
+    let mut user_input = String::new();
+    println!("Input Number");
+    get_user_input(&mut user_input);
 }
+
+fn get_user_input(buffer: &mut String) {
+    buffer.clear();
+    match io::stdin().read_line(buffer) {
+        Ok(_) => {},
+        Err(err) => println!("{}", err)
+    }
+} 
